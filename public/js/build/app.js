a77c0f757d0a805094f78d8bf5ccbe8018402123
Sergio Somma <sdsomma@gmail.com>
2018-08-17 12:45:50 -0700

    import react and build

--- public/js/build/app.js
+++ public/js/build/app.js
@@ -346,7 +346,7 @@ eval("\n\nif (false) {} else {\n  module.exports = __webpack_require__(/*! ./cjs
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"../../node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_reactDom2.default.render(React.createElement(\n  \"h1\",\n  null,\n  \"Hello\"\n), document.getElementById('app'));\n\n//# sourceURL=webpack:///./app.js?");
+eval("\n\nvar _react = __webpack_require__(/*! react */ \"../../node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"../../node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_reactDom2.default.render(_react2.default.createElement(\n  'h1',\n  null,\n  'Hello'\n), document.getElementById('app'));\n\n//# sourceURL=webpack:///./app.js?");
 
 /***/ })
 
