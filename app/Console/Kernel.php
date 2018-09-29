433a22000870524976d5494621327da3717ec8b0
Jan Hartigan <janhartigan13@gmail.com>
2018-09-29 14:29:39 -0700

    small cleanup from project init

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace App\Console;
+<?php namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace App\Exceptions;
+<?php namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Validation\ValidationException;
@@ -11,40 +9,20 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        AuthorizationException::class,
-        HttpException::class,
-        ModelNotFoundException::class,
-        ValidationException::class,
-    ];
+	protected $dontReport = [
+		AuthorizationException::class,
+		HttpException::class,
+		ModelNotFoundException::class,
+		ValidationException::class,
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
-     */
-    public function report(Exception $e)
-    {
-        parent::report($e);
-    }
+	public function report(Exception $e)
+	{
+		parent::report($e);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $e)
-    {
-        return parent::render($request, $e);
-    }
+	public function render($request, Exception $e)
+	{
+		return parent::render($request, $e);
+	}
 }
