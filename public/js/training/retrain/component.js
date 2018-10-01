7a863bd25638ff7311a42ea87ef459d832da4828
Sergio Somma <sdsomma@gmail.com>
2018-10-01 00:26:16 -0700

    seconds limit input

--- public/js/training/retrain/component.js
+++ public/js/training/retrain/component.js
@@ -3,16 +3,41 @@ import axios from 'axios'
 import React, { Component } from "react"
 
 class Retrain extends Component {
+
+	constructor(props)
+	{
+		super(props);
+		this.state = {secondsLimit: 500};
+	}
+
 	render() {
 		return (
 			<div className="cmp-retrain">
+				<div>
+					<div><label>Seconds Limit</label></div>
+					<div>
+						<input
+						   type="text"
+						   value={this.state.secondsLimit}
+						   onChange={this.handleSecondsLimitChange.bind(this)}/>
+					</div>
+				</div>
 				<button onClick={this.retrain} className="c-button c-button--big c-button--gray">Retrain the AI</button>
 			</div>
 		);
 	}
 
+	handleSecondsLimitChange(event)
+	{
+		this.setState({secondsLimit: event.target.value});
+	}
+
 	retrain = () => {
-		return axios.post('/training/session', {secondsLimit: 700}).then(response => this.props.history.push('/sessions'));
+		let data = {
+			secondsLimit: this.state.secondsLimit,
+		};
+
+		return axios.post('/training/session', data).then(response => this.props.history.push('/sessions'));
 	}
 }
 
