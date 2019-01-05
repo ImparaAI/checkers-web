7676c210add15e561c680ca372d014692303632d
Jan Hartigan <janhartigan13@gmail.com>
2019-01-04 20:32:01 -0800

    cleaning up the training session ui a bit

--- public/js/training/sessions/component.js
+++ public/js/training/sessions/component.js
@@ -1,5 +1,6 @@
 import "./style.scss"
 import axios from 'axios'
+import moment from 'moment'
 import React, { Component } from "react"
 
 class TrainingSessions extends Component {
@@ -23,11 +24,12 @@ class TrainingSessions extends Component {
 					<thead>
 						<tr>
 							<th>Name</th>
-							<th>Seconds Limit</th>
+							<th>Limit (sec)</th>
 							<th>Episodes</th>
 							<th>Created</th>
-							<th>Start Time</th>
-							<th>End Time</th>
+							<th>Started</th>
+							<th>Last Trained</th>
+							<th>Ended</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -43,18 +45,33 @@ class TrainingSessions extends Component {
 		return this.state.sessions.map(session =>
 		(
 			<tr key={session.id} className="cmp-sessions__row">
-				<td>{session.name}</td>
+				<td className="cmp-sessions__row-name">{session.name}</td>
 				<td>{session.secondsLimit}</td>
 				<td>{session.episodeCount}</td>
-				<td>{session.createdAt}</td>
-				<td>{session.startTime}</td>
-				<td>{session.endTime}</td>
+				<td title={this.formatDate(session.createdAt)}>{this.formatDateFromNow(session.createdAt)}</td>
+				<td title={this.formatDate(session.startTime)}>{this.formatDateFromNow(session.startTime)}</td>
+				<td title={this.formatDate(session.latestLessonTime)}>{this.formatDateFromNow(session.latestLessonTime)}</td>
+				<td title={this.formatDate(session.endTime)}>{this.formatDateFromNow(session.endTime)}</td>
 			</tr>
 		));
 	}
 
 	loadSessions() {
 		return axios.get('/training/sessions').then(response => this.setState({sessions: response.data.sessions}));
+	}
+
+	formatDate(value) {
+		if (!value)
+			return '-';
+
+		return moment.utc(value).local().format('LLL');
+	}
+
+	formatDateFromNow(value) {
+		if (!value)
+			return '-';
+
+		return moment.utc(value).local().calendar();
 	}
 }
 
